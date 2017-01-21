a0f7d310f29b1813fb51cf77e527b47da858f7fc
atil <hyperion91@gmail.com>
2017-01-21 16:25:40 +0300

    Sfx baseline;

---
+++ Waves/Assets/Scripts/Sfx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public static class Sfx
+{
+    public static T Random<T>(this IList<T> lst)
+    {
+        return lst[UnityEngine.Random.Range(0, lst.Count)];
+    }
+
+    public static AudioClip[] WheelSpins;
+    public static AudioClip[] WheelSelects;
+    public static AudioClip[] Countdowns;
+    public static AudioClip[] SequenceCompletedLights;
+    public static AudioClip[] SequenceCompletedMediums;
+    public static AudioClip[] SequenceCompletedHeavys;
+    public static AudioClip[] GirmeyeYaklastis;
+    public static AudioClip[] BonusCompleteds;
+    public static AudioClip[] Girdis;
+
+    private static readonly AudioSource AudioSource;
+
+    static Sfx()
+    {
+        AudioSource = Camera.main.GetComponent<AudioSource>();
+
+        WheelSpins = new[]
+        {
+            Resources.Load<AudioClip>("Sfx/WheelSpins/WheelSpin1"),
+            Resources.Load<AudioClip>("Sfx/WheelSpins/WheelSpin2"),
+            Resources.Load<AudioClip>("Sfx/WheelSpins/WheelSpin3"),
+        };
+
+        WheelSelects = new[]
+        {
+            Resources.Load<AudioClip>("Sfx/WheelSpins/WheelSelects"),
+            Resources.Load<AudioClip>("Sfx/WheelSpins/WheelSelects"),
+            Resources.Load<AudioClip>("Sfx/WheelSpins/WheelSelects"),
+        };
+
+        // ...
+
+    }
+
+    public static void PlayWheelSpin()
+    {
+        AudioSource.PlayOneShot(WheelSpins.Random());
+    }
+}
